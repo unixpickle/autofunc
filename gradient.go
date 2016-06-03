3c4221d990091163755e1b16e7ef9785272c5968
Alex Nichol <unixpickle@gmail.com>
2016-06-03 18:15:30 -0400

    created helpers for creating *Gradients

--- gradient.go
+++ gradient.go
@@ -7,10 +7,22 @@ import "github.com/unixpickle/num-analysis/linalg"
 // of various variables.
 type Gradient map[*Variable]linalg.Vector
 
+func NewGradient(vars []*Variable) Gradient {
+	res := Gradient{}
+	for _, v := range vars {
+		res[v] = make(linalg.Vector, len(v.Vector))
+	}
+	return res
+}
+
 // An RGradient is like a Gradient, but its entries
 // correspond to the derivatives of the components
 // of the gradient with respect to a variable r.
 type RGradient map[*Variable]linalg.Vector
+
+func NewRGradient(vars []*Variable) RGradient {
+	return RGradient(NewGradient(vars))
+}
 
 // An RVector specifies how much each variable
 // changes with respect to r for a given r-operator
