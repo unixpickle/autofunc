44752a44eb74adad72dd5ed5d184b92709c2d210
Alex Nichol <unixpickle@gmail.com>
2016-06-10 14:30:53 -0400

    drafted tests for LinTran.Batch and LinTran.BatchR

--- lin_tran_test.go
+++ lin_tran_test.go
@@ -29,8 +29,12 @@ var linTranTestVec = &Variable{
 	Vector: linalg.Vector([]float64{4, 3, 2, 1}),
 }
 
+var linTranTestVec2 = &Variable{
+	Vector: linalg.Vector([]float64{5, 1, -3, 2, 1, 2, 3, 4}),
+}
+
 var linTranTestVariables = []*Variable{
-	linTranTestMat1.Data, linTranTestMat2.Data, linTranTestVec,
+	linTranTestMat1.Data, linTranTestMat2.Data, linTranTestVec, linTranTestVec2,
 }
 
 var linTranTestRVec = RVector{
@@ -42,7 +46,19 @@ var linTranTestRVec = RVector{
 	linTranTestMat2.Data: linalg.Vector([]float64{
 		0.45823, 0.66692, 0.14662,
 	}),
-	linTranTestVec: linalg.Vector([]float64{0, -1, 3, -7}),
+	linTranTestVec:  linalg.Vector([]float64{0, -1, 3, -7}),
+	linTranTestVec2: linalg.Vector([]float64{1, 3, -2, 3, -3, 2, 1, 7}),
+}
+
+type linTranBatchTest struct{}
+
+func (_ linTranBatchTest) Apply(v Result) Result {
+	return AddTwice{}.Apply(linTranTestMat2.Batch(linTranTestMat1.Batch(v, 2), 2))
+}
+
+func (_ linTranBatchTest) ApplyR(rv RVector, v RResult) RResult {
+	batchOut := linTranTestMat2.BatchR(rv, linTranTestMat1.BatchR(rv, v, 2), 2)
+	return AddTwice{}.ApplyR(rv, batchOut)
 }
 
 func TestLinTranOutput(t *testing.T) {
@@ -69,6 +85,39 @@ func TestLinTranRGradient(t *testing.T) {
 		F:     ComposedRFunc{linTranTestMat1, linTranTestMat2, AddTwice{}},
 		Vars:  linTranTestVariables,
 		Input: linTranTestVec,
+		RV:    linTranTestRVec,
+	}
+	funcTest.Run(t)
+}
+
+func TestLinTranBatchOutput(t *testing.T) {
+	actual := linTranBatchTest{}.Apply(linTranTestVec2).Output()
+	expected := []float64{349 * 2, 131 * 2}
+	if len(actual) != len(expected) {
+		t.Fatalf("invalid output length (expected %d got %d)", len(expected), len(actual))
+	}
+	for i, x := range expected {
+		a := actual[i]
+		if math.Abs(a-x) > 1e-5 {
+			t.Errorf("output %d should be %f but its %f", i, x, a)
+		}
+	}
+}
+
+func TestLinTranBatchGradient(t *testing.T) {
+	funcTest := &FuncTest{
+		F:     linTranBatchTest{},
+		Vars:  linTranTestVariables,
+		Input: linTranTestVec2,
+	}
+	funcTest.Run(t)
+}
+
+func TestLinTranBatchRGradient(t *testing.T) {
+	funcTest := &RFuncTest{
+		F:     linTranBatchTest{},
+		Vars:  linTranTestVariables,
+		Input: linTranTestVec2,
 		RV:    linTranTestRVec,
 	}
 	funcTest.Run(t)
