89ac63d4e14ede44b065f3908d10e5befa01f878
Alex Nichol <unixpickle@gmail.com>
2016-09-15 10:12:20 -0400

    implemented outer products

--- lin_alg.go
+++ lin_alg.go
@@ -1,6 +1,10 @@
 package autofunc
 
-import "github.com/unixpickle/num-analysis/linalg"
+import (
+	"github.com/gonum/blas"
+	"github.com/gonum/blas/blas64"
+	"github.com/unixpickle/num-analysis/linalg"
+)
 
 type matMulResult struct {
 	MatIn  Result
@@ -101,5 +105,198 @@ func (m *matMulRResult) PropagateRGradient(upstream, upstreamR linalg.Vector,
 		delete(grad, m.MatVar)
 		delete(rgrad, m.MatVar)
 		m.MatIn.PropagateRGradient(downstream, downstreamR, rgrad, grad)
+	}
+}
+
+type outerProductResult struct {
+	OutputVec linalg.Vector
+	LeftIn    Result
+	RightIn   Result
+}
+
+// OuterProduct computes the outer product between two
+// vectors, expressed as left*transpose(right) where both
+// vectors are column vectors.
+func OuterProduct(left, right Result) Result {
+	outMat := blas64.General{
+		Data:   make([]float64, len(left.Output())*len(right.Output())),
+		Rows:   len(left.Output()),
+		Cols:   len(right.Output()),
+		Stride: len(right.Output()),
+	}
+	leftVec := blas64.Vector{
+		Data: left.Output(),
+		Inc:  1,
+	}
+	rightVec := blas64.Vector{
+		Data: right.Output(),
+		Inc:  1,
+	}
+	blas64.Ger(1, leftVec, rightVec, outMat)
+	return &outerProductResult{
+		OutputVec: outMat.Data,
+		LeftIn:    left,
+		RightIn:   right,
+	}
+}
+
+func (o *outerProductResult) Output() linalg.Vector {
+	return o.OutputVec
+}
+
+func (o *outerProductResult) Constant(g Gradient) bool {
+	return o.LeftIn.Constant(g) && o.RightIn.Constant(g)
+}
+
+func (o *outerProductResult) PropagateGradient(upstream linalg.Vector, grad Gradient) {
+	upstreamMatrix := blas64.General{
+		Data:   upstream,
+		Rows:   len(o.LeftIn.Output()),
+		Cols:   len(o.RightIn.Output()),
+		Stride: len(o.RightIn.Output()),
+	}
+	if !o.LeftIn.Constant(grad) {
+		rightVec := blas64.Vector{
+			Data: o.RightIn.Output(),
+			Inc:  1,
+		}
+		leftDownstream := blas64.Vector{
+			Data: make(linalg.Vector, len(o.LeftIn.Output())),
+			Inc:  1,
+		}
+		blas64.Gemv(blas.NoTrans, 1, upstreamMatrix, rightVec, 0, leftDownstream)
+		o.LeftIn.PropagateGradient(leftDownstream.Data, grad)
+	}
+	if !o.RightIn.Constant(grad) {
+		leftVec := blas64.Vector{
+			Data: o.LeftIn.Output(),
+			Inc:  1,
+		}
+		rightDownstream := blas64.Vector{
+			Data: make(linalg.Vector, len(o.RightIn.Output())),
+			Inc:  1,
+		}
+		blas64.Gemv(blas.Trans, 1, upstreamMatrix, leftVec, 0, rightDownstream)
+		o.RightIn.PropagateGradient(rightDownstream.Data, grad)
+	}
+}
+
+type outerProductRResult struct {
+	OutputVec  linalg.Vector
+	ROutputVec linalg.Vector
+	LeftIn     RResult
+	RightIn    RResult
+}
+
+// OuterProductR is like OuterProduct but for RResults.
+func OuterProductR(left, right RResult) RResult {
+	outMat := blas64.General{
+		Data:   make([]float64, len(left.Output())*len(right.Output())),
+		Rows:   len(left.Output()),
+		Cols:   len(right.Output()),
+		Stride: len(right.Output()),
+	}
+	outMatR := outMat
+	outMatR.Data = make([]float64, len(outMat.Data))
+
+	leftVec := blas64.Vector{
+		Data: left.Output(),
+		Inc:  1,
+	}
+	leftVecR := blas64.Vector{
+		Data: left.ROutput(),
+		Inc:  1,
+	}
+	rightVec := blas64.Vector{
+		Data: right.Output(),
+		Inc:  1,
+	}
+	rightVecR := blas64.Vector{
+		Data: right.ROutput(),
+		Inc:  1,
+	}
+
+	blas64.Ger(1, leftVec, rightVec, outMat)
+	blas64.Ger(1, leftVecR, rightVec, outMatR)
+	blas64.Ger(1, leftVec, rightVecR, outMatR)
+	return &outerProductRResult{
+		OutputVec:  outMat.Data,
+		ROutputVec: outMatR.Data,
+		LeftIn:     left,
+		RightIn:    right,
+	}
+}
+
+func (o *outerProductRResult) Output() linalg.Vector {
+	return o.OutputVec
+}
+
+func (o *outerProductRResult) ROutput() linalg.Vector {
+	return o.ROutputVec
+}
+
+func (o *outerProductRResult) Constant(rg RGradient, g Gradient) bool {
+	return o.LeftIn.Constant(rg, g) && o.RightIn.Constant(rg, g)
+}
+
+func (o *outerProductRResult) PropagateRGradient(upstream, upstreamR linalg.Vector,
+	rgrad RGradient, grad Gradient) {
+	upstreamMatrix := blas64.General{
+		Data:   upstream,
+		Rows:   len(o.LeftIn.Output()),
+		Cols:   len(o.RightIn.Output()),
+		Stride: len(o.RightIn.Output()),
+	}
+	upstreamMatrixR := blas64.General{
+		Data:   upstreamR,
+		Rows:   len(o.LeftIn.Output()),
+		Cols:   len(o.RightIn.Output()),
+		Stride: len(o.RightIn.Output()),
+	}
+	if !o.LeftIn.Constant(rgrad, grad) {
+		rightVec := blas64.Vector{
+			Data: o.RightIn.Output(),
+			Inc:  1,
+		}
+		rightVecR := blas64.Vector{
+			Data: o.RightIn.ROutput(),
+			Inc:  1,
+		}
+		leftDownstream := blas64.Vector{
+			Data: make(linalg.Vector, len(o.LeftIn.Output())),
+			Inc:  1,
+		}
+		leftDownstreamR := blas64.Vector{
+			Data: make(linalg.Vector, len(o.LeftIn.Output())),
+			Inc:  1,
+		}
+		blas64.Gemv(blas.NoTrans, 1, upstreamMatrix, rightVec, 0, leftDownstream)
+		blas64.Gemv(blas.NoTrans, 1, upstreamMatrix, rightVecR, 0, leftDownstreamR)
+		blas64.Gemv(blas.NoTrans, 1, upstreamMatrixR, rightVec, 1, leftDownstreamR)
+		o.LeftIn.PropagateRGradient(leftDownstream.Data, leftDownstreamR.Data,
+			rgrad, grad)
+	}
+	if !o.RightIn.Constant(rgrad, grad) {
+		leftVec := blas64.Vector{
+			Data: o.LeftIn.Output(),
+			Inc:  1,
+		}
+		leftVecR := blas64.Vector{
+			Data: o.LeftIn.ROutput(),
+			Inc:  1,
+		}
+		rightDownstream := blas64.Vector{
+			Data: make(linalg.Vector, len(o.RightIn.Output())),
+			Inc:  1,
+		}
+		rightDownstreamR := blas64.Vector{
+			Data: make(linalg.Vector, len(o.RightIn.Output())),
+			Inc:  1,
+		}
+		blas64.Gemv(blas.Trans, 1, upstreamMatrix, leftVec, 0, rightDownstream)
+		blas64.Gemv(blas.Trans, 1, upstreamMatrix, leftVecR, 0, rightDownstreamR)
+		blas64.Gemv(blas.Trans, 1, upstreamMatrixR, leftVec, 1, rightDownstreamR)
+		o.RightIn.PropagateRGradient(rightDownstream.Data, rightDownstreamR.Data,
+			rgrad, grad)
 	}
 }
--- lin_alg_test.go
+++ lin_alg_test.go
@@ -16,6 +16,17 @@ func (_ matMulVecTest) ApplyR(rv RVector, in RResult) RResult {
 	return MatMulVecR(mat, 3, 4, in)
 }
 
+type outerProductTest struct{}
+
+func (_ outerProductTest) Apply(in Result) Result {
+	return OuterProduct(linTranTestVec, in)
+}
+
+func (_ outerProductTest) ApplyR(rv RVector, in RResult) RResult {
+	vec1 := NewRVariable(linTranTestVec, rv)
+	return OuterProductR(vec1, in)
+}
+
 func TestMatMulVecOutput(t *testing.T) {
 	res := matMulVecTest{}.Apply(linTranTestVec)
 	expected := []float64{19, 20, 36}
@@ -40,6 +51,40 @@ func TestMatMulVecRGradient(t *testing.T) {
 		F:     ComposedRFunc{matMulVecTest{}, AddTwice{}},
 		Vars:  linTranTestVariables,
 		Input: linTranTestVec,
+		RV:    linTranTestRVec,
+	}
+	funcTest.Run(t)
+}
+
+func TestOuterProductOutput(t *testing.T) {
+	res := outerProductTest{}.Apply(linTranTestMat2.Data)
+	expected := []float64{
+		16, 8, 12,
+		12, 6, 9,
+		8, 4, 6,
+		4, 2, 3,
+	}
+	for i, x := range expected {
+		if math.Abs(x-res.Output()[i]) > funcTestPrec {
+			t.Errorf("bad output %d: expected %f got %f", i, x, res.Output()[i])
+		}
+	}
+}
+
+func TestOuterProductGradient(t *testing.T) {
+	funcTest := &FuncTest{
+		F:     ComposedFunc{outerProductTest{}, AddTwice{}},
+		Vars:  linTranTestVariables,
+		Input: linTranTestMat2.Data,
+	}
+	funcTest.Run(t)
+}
+
+func TestOuterProductRGradient(t *testing.T) {
+	funcTest := &RFuncTest{
+		F:     ComposedRFunc{outerProductTest{}, AddTwice{}},
+		Vars:  linTranTestVariables,
+		Input: linTranTestMat2.Data,
 		RV:    linTranTestRVec,
 	}
 	funcTest.Run(t)
