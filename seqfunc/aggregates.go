84a826fe1e853648f5518eebbda50f485aeb84a7
Alex Nichol <unixpickle@gmail.com>
2016-12-08 15:31:13 -0500

    implemented seqfunc.ConcatAll utility
    This will mostly be useful for turning a seqfunc.Func into an autofunc.Func.

--- seqfunc/aggregates.go
+++ seqfunc/aggregates.go
@@ -59,6 +59,35 @@ func MeanR(in RResult) autofunc.RResult {
 	return autofunc.ScaleR(sum, 1/float64(count(in.OutputSeqs())))
 }
 
+// ConcatAll joins all of the timesteps in all of the
+// sequences in to one packed autofunc.Result.
+// The packing is done as follows: first timesteps from
+// the same sequence are packed left to right, then the
+// packed vectors from each sequence are joined together
+// from the first sequence to the last.
+func ConcatAll(in Result) autofunc.Result {
+	var joined linalg.Vector
+	for _, seq := range in.OutputSeqs() {
+		for _, vec := range seq {
+			joined = append(joined, vec...)
+		}
+	}
+	return &concatAllResult{Input: in, OutVec: joined}
+}
+
+// ConcatAllR is like ConcatAll for RResults.
+func ConcatAllR(in RResult) autofunc.RResult {
+	var joined, joinedR linalg.Vector
+	rOut := in.ROutputSeqs()
+	for i, seq := range in.OutputSeqs() {
+		for j, vec := range seq {
+			joined = append(joined, vec...)
+			joinedR = append(joinedR, rOut[i][j]...)
+		}
+	}
+	return &concatAllRResult{Input: in, OutVec: joined, ROutVec: joinedR}
+}
+
 func count(s [][]linalg.Vector) int {
 	var res int
 	for _, seq := range s {
@@ -122,4 +151,66 @@ func (a *addAllRResult) PropagateRGradient(u, uR linalg.Vector, rg autofunc.RGra
 		}
 	}
 	a.In.PropagateRGradient(upSeqs, upSeqsR, rg, g)
+}
+
+type concatAllResult struct {
+	Input  Result
+	OutVec linalg.Vector
+}
+
+func (a *concatAllResult) Output() linalg.Vector {
+	return a.OutVec
+}
+
+func (a *concatAllResult) Constant(g autofunc.Gradient) bool {
+	return false
+}
+
+func (a *concatAllResult) PropagateGradient(u linalg.Vector, g autofunc.Gradient) {
+	var idx int
+	var splitUpstream [][]linalg.Vector
+	for _, outSeq := range a.Input.OutputSeqs() {
+		var splitSeq []linalg.Vector
+		for _, step := range outSeq {
+			splitSeq = append(splitSeq, u[idx:idx+len(step)])
+			idx += len(step)
+		}
+		splitUpstream = append(splitUpstream, splitSeq)
+	}
+	a.Input.PropagateGradient(splitUpstream, g)
+}
+
+type concatAllRResult struct {
+	Input   RResult
+	OutVec  linalg.Vector
+	ROutVec linalg.Vector
+}
+
+func (a *concatAllRResult) Output() linalg.Vector {
+	return a.OutVec
+}
+
+func (a *concatAllRResult) ROutput() linalg.Vector {
+	return a.ROutVec
+}
+
+func (a *concatAllRResult) Constant(rg autofunc.RGradient, g autofunc.Gradient) bool {
+	return false
+}
+
+func (a *concatAllRResult) PropagateRGradient(u, uR linalg.Vector, rg autofunc.RGradient,
+	g autofunc.Gradient) {
+	var idx int
+	var splitUpstream, splitUpstreamR [][]linalg.Vector
+	for _, outSeq := range a.Input.OutputSeqs() {
+		var splitSeq, splitSeqR []linalg.Vector
+		for _, step := range outSeq {
+			splitSeq = append(splitSeq, u[idx:idx+len(step)])
+			splitSeqR = append(splitSeqR, uR[idx:idx+len(step)])
+			idx += len(step)
+		}
+		splitUpstream = append(splitUpstream, splitSeq)
+		splitUpstreamR = append(splitUpstreamR, splitSeqR)
+	}
+	a.Input.PropagateRGradient(splitUpstream, splitUpstreamR, rg, g)
 }
--- seqfunc/test/aggregates_test.go
+++ seqfunc/test/aggregates_test.go
@@ -20,6 +20,18 @@ func (p *AddAllTestFunc) ApplyR(rv autofunc.RVector, in autofunc.RResult) autofu
 	return seqfunc.AddAllR(seqfunc.VarRResult(rv, TestSeqs))
 }
 
+type ConcatAllTestFunc struct{}
+
+func (p *ConcatAllTestFunc) Apply(in autofunc.Result) autofunc.Result {
+	s := seqfunc.VarResult(TestSeqs)
+	return autofunc.Concat(in, seqfunc.ConcatAll(s))
+}
+
+func (p *ConcatAllTestFunc) ApplyR(rv autofunc.RVector, in autofunc.RResult) autofunc.RResult {
+	s := seqfunc.VarRResult(rv, TestSeqs)
+	return autofunc.ConcatR(in, seqfunc.ConcatAllR(s))
+}
+
 func TestAddAllOutput(t *testing.T) {
 	in := [][]linalg.Vector{
 		{{1, 2}, {3, -1}},
@@ -35,6 +47,34 @@ func TestAddAllOutput(t *testing.T) {
 func TestAddAll(t *testing.T) {
 	checker := &functest.RFuncChecker{
 		F:     &AddAllTestFunc{},
+		Input: TestVars[0],
+		Vars:  TestVars,
+		RV:    TestRV,
+	}
+	checker.FullCheck(t)
+}
+
+func TestConcatAllOutput(t *testing.T) {
+	in := [][]linalg.Vector{
+		{{1, 2}, {3, 4}},
+		{{5}, {6, 7, 8}, {9}},
+		{},
+		{{10, 11, 12, 13, 14, 15}},
+	}
+	inSeq := seqfunc.ConstResult(in)
+	actual := seqfunc.ConcatAll(inSeq).Output()
+	expected := make(linalg.Vector, 15)
+	for i := range expected {
+		expected[i] = float64(i) + 1
+	}
+	if expected.Copy().Scale(-1).Add(actual).MaxAbs() > 1e-6 {
+		t.Errorf("expected %v got %v", expected, actual)
+	}
+}
+
+func TestConcatAll(t *testing.T) {
+	checker := &functest.RFuncChecker{
+		F:     &ConcatAllTestFunc{},
 		Input: TestVars[0],
 		Vars:  TestVars,
 		RV:    TestRV,
